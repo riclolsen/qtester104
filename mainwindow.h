f94fa4ebd372e4fa5a75b27339f9e055d5faf6b2
Ricardo Olsen <olsen.ricardo@gmail.com>
2025-05-12 09:00:14 -0300

    Version 2.7.2.

--- mainwindow.h
+++ mainwindow.h
@@ -42,7 +42,7 @@
 #include "iec104_class.h"
 #include "qiec104.h"
 
-#define QTESTER_VERSION "v2.7.1"
+#define QTESTER_VERSION "v2.7.2"
 #define QTESTER_COPYRIGHT "Copyright © 2010-2025 Ricardo Lastra Olsen"
 #define CURDIRINIFILENAME "/qtester104.ini"
 #define CONFDIRINIFILENAME "../conf/qtester104.ini"
